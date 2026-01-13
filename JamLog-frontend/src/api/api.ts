f65dd4bcb6b55200f8cf534462d7dfd7f259b5a4
Milian <brytechannel@gmail.com>
2026-01-13 14:45:45 +0000

    updated for production

--- JamLog-frontend/src/api/api.ts
+++ JamLog-frontend/src/api/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://157.26.174.59:3000",
+  baseURL: "http://192.168.100.145:3000",
   headers: {
-    "Content-Type": "application/json",
-  },
+    "Content-Type": "application/json"
+  }
 });
 
 export default api;
